46c518acbcd499c79f1574c8316ef2278cd4fb74
tle2693 <toanle2612@outlook.com>
2020-07-28 13:45:49 -0400

    Empty Project

---
+++ UnitConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Toan Le on 7/28/20.
+//  Copyright © 2020 TL. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
